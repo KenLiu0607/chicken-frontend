c563265d6ab0bc249e3a012904aa21e5e123a5be
Ken_Liu <ken.liu@yfy.com>
2025-12-24 11:38:39 +0800

    調整vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/chicken-frontend", // TODO: 將 YOUR_REPO_NAME 替換為您的 GitHub 儲存庫名稱
+    base: "/chicken-frontend/", // TODO: 將 YOUR_REPO_NAME 替換為您的 GitHub 儲存庫名稱
 });
